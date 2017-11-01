85106068695d9d36497062ff192003fa96b97b30
Scott Donaldson <scott.p.donaldson@gmail.com>
2017-10-31 23:36:37 -0400

    update instructions

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -16,7 +16,7 @@ export default class Instructions extends Component {
         <p><b>Click</b> to add a voxel to the world. You can also <b>shift + click</b> to remove a voxel.</p>
         <p><b>Click and drag</b> to rotate the camera. <b>Right-click and drag</b> to pan.</p>
         <p>In the controls on the left, you can change the color, or choose a color from an object in the world.</p>
-        <p>You can also switch between adding voxels and adding spheres to the world.</p>
+        <p>You can also switch between adding voxels, spheres, or beams to the world.</p>
         <p>Click <b>View History</b> to replay the world's construction in the order the objects were added. Toggle <b>View By Player</b> on or off to see objects colored by the player who added them to the world.</p>
         <p>Questions? Feedback? Let me know: <a href="mailto:scott.p.donaldson@gmail.com">scott.p.donaldson@gmail.com</a></p>
       </div>
