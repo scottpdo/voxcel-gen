a14f800f6b7255a3d09692706120d14154d05c15
Scott Donaldson <scott.p.donaldson@gmail.com>
2017-11-05 11:09:03 -0500

    password protected worlds

--- src/components/Worlds.js
+++ src/components/Worlds.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import * as firebase from 'firebase';
 import _ from 'lodash';
 import slugify from 'slugify';
+import md5 from 'md5';
 
 import '../css/Worlds.css';
 
@@ -15,26 +16,35 @@ type Props = {
 
 type Pair = {
   name: string,
-  screenshot: ?string
+  screenshot: ?string,
+  slug: string
 };
 
 type State = {
+  isShiftDown: boolean,
   worlds: Array<Pair>
 };
 
 export default class Worlds extends Component<Props, State> {
 
   dataRef: firebase.ref;
+  onKeyDown: Function;
+  onKeyUp: Function;
   showWorlds: Function;
+
+  static SHIFT = 16;
 
   constructor() {
     
     super();
     
     this.state = {
+      isShiftDown: false,
       worlds: []
     };
 
+    this.onKeyDown = this.onKeyDown.bind(this);
+    this.onKeyUp = this.onKeyUp.bind(this);
     this.showWorlds = this.showWorlds.bind(this);
   }
 
@@ -43,15 +53,21 @@ export default class Worlds extends Component<Props, State> {
     const worlds = [];
 
     snapshot.forEach(child => {
+
+      const value = child.val();
+      const slug = child.key;
+      const name = _.isString(value) ? value : value.name;
+
       worlds.push({
-        name: child.key,
-        screenshot: null
+        name,
+        screenshot: null,
+        slug
       });
     });
 
     worlds.forEach(world => {
 
-      const ref = this.props.storage.ref('images/' + world.name);
+      const ref = this.props.storage.ref('images/' + world.slug);
 
       ref.getDownloadURL().then(url => {
         world.screenshot = url;
@@ -65,15 +81,29 @@ export default class Worlds extends Component<Props, State> {
   componentDidMount() {
     this.dataRef = this.props.db.ref('worldIndex');
     this.dataRef.on('value', this.showWorlds);
+
+    window.addEventListener('keydown', this.onKeyDown);
+    window.addEventListener('keyup', this.onKeyUp);
   }
 
   componentWillUnMount() {
     this.dataRef.off();
+    window.removeEventListener('keydown', this.onKeyDown);
+    window.removeEventListener('keyup', this.onKeyUp);
   }
 
-  addWorld(message: string) {
+  onKeyDown(e: KeyboardEvent) {
+    if (e.keyCode === Worlds.SHIFT) this.setState({ isShiftDown: true });
+  }
+
+  onKeyUp(e: KeyboardEvent) {
+    if (e.keyCode === Worlds.SHIFT) this.setState({ isShiftDown: false });
+  }
+
+  addWorld(message: string, e: MouseEvent) {
 
     const name = prompt(message);
+    let password = null;
 
     if (name === "" || _.isNil(name)) return;
 
@@ -85,16 +115,25 @@ export default class Worlds extends Component<Props, State> {
 
     if (slug === "") return;
 
+    if (e.shiftKey) {
+      password = prompt("What is the password to your world?");
+      
+      if (password === "" || _.isNil(password)) 
+        return alert("That's not a valid password. Please try again.");
+      
+      password = md5(password);
+    }
+
     this.dataRef.child(slug).once('value', snapshot => {
       
       const val = snapshot.val();
 
       // if name not taken, add it to the worlds
       if (_.isNil(val)) {
-        this.dataRef.child(slug).set(name);
+        this.dataRef.child(slug).set({ name, password });
       // otherwise, ask for a new name
       } else {
-        this.addWorld("There is already a world with that name. Try another!");
+        this.addWorld("There is already a world with that name. Try another!", e);
       }
     });
   }
@@ -110,19 +149,23 @@ export default class Worlds extends Component<Props, State> {
       }
 
       return (
-      <li className="worlds__world" key={"world-" + i}>
-        <Link to={"/world/" + world.name}>
-          <div className="worlds__img" style={style}></div>
-          <span>{world.name}</span>
-        </Link>
-      </li>
+        <li className="worlds__world" key={"world-" + i}>
+          <Link to={"/world/" + world.slug}>
+            <div className="worlds__img" style={style}></div>
+            <span>{world.name || "(Unnamed)"}</span>
+          </Link>
+        </li>
       );
     });
+
+    let addNewText = "Add New";
+    if (this.state.isShiftDown)
+      addNewText += " [with Password]";
 
     return (
       <div className="worlds">
         <h2>
-          Worlds: (<span onClick={this.addWorld.bind(this, "What is your world's name?")} className="worlds__addnew">Add New</span>)
+          Worlds: (<span onClick={this.addWorld.bind(this, "What is your world's name?")} className="worlds__addnew">{addNewText}</span>)
           <Link to="/instructions" className="worlds__instructions">Instructions</Link>
         </h2>
         <ul className="worlds__ul">{worlds}</ul>
